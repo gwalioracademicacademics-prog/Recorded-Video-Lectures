451c785685cb456a9b938370bf51c3a970231a7c
gwalioracademicacademics-prog <gwalioracademicacademics@gmail.com>
2026-05-21 14:16:16 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // ================================================================
 //  data.js — Unacademy Gwalior Database
-//  Generated: 05/05/2026, 17:49:05
+//  Generated: 21/05/2026, 14:15:18
 //  Paste this entire file into data.js on GitHub and commit.
 // ================================================================
 
@@ -10,6 +10,38 @@ const DB = {
     {
         "id": "moslc5mhl5rd",
         "name": "IIT-JEE Excel-1 (Morning)"
+    },
+    {
+        "id": "mpf8lfvkcjdj",
+        "name": "IIT-JEE Excel-2 (Evening)"
+    },
+    {
+        "id": "mpf8lr5idez8",
+        "name": "IIT-JEE Growth-1"
+    },
+    {
+        "id": "mpf8lvjd8x6n",
+        "name": "NEET Excel-1"
+    },
+    {
+        "id": "mpf8m00h475g",
+        "name": "NEET Growth-1"
+    },
+    {
+        "id": "mpf8m5xgxy9t",
+        "name": "NEET Conquer-1"
+    },
+    {
+        "id": "mpf8mdqx8ov3",
+        "name": "IIT-JEE Conquer-1"
+    },
+    {
+        "id": "mpf8mi1kdw37",
+        "name": "Foundation Elevate-1"
+    },
+    {
+        "id": "mpf8mm5qdkpy",
+        "name": "Foundation Adapt-1"
     }
 ],
 
@@ -21,25 +53,81 @@ const DB = {
         "batchId": "moslc5mhl5rd",
         "active": "true",
         "password": "123"
+    },
+    {
+        "id": "mpf8p7jtdr60",
+        "name": "Ashutosh Maurya",
+        "email": "ashutosh@unacademy.com",
+        "batchId": "mpf8m00h475g",
+        "active": "true",
+        "password": "12345"
+    },
+    {
+        "id": "mpf8q11emnc1",
+        "name": "Akhand Pratap Singh",
+        "email": "akhand@unacademy.com",
+        "batchId": "mpf8m00h475g",
+        "active": "true",
+        "password": "12345"
+    },
+    {
+        "id": "mpf8qdxrgstl",
+        "name": "Mayur Suthar",
+        "email": "mayur@unacademy.com",
+        "batchId": "mpf8m00h475g",
+        "active": "true",
+        "password": "12345"
+    },
+    {
+        "id": "mpf8rfjhvvoj",
+        "name": "Kumar Manglam",
+        "email": "manglam@unacademy.com",
+        "batchId": "mpf8m00h475g",
+        "active": "true",
+        "password": "12345"
     }
 ],
 
   lectures: [
     {
-        "id": "moslhqhzhl2o",
-        "title": "demo2",
-        "subject": "demo2",
-        "ytId": "RJF5LrP8eSw",
-        "batchId": "moslc5mhl5rd",
-        "date": "2026-05-05"
+        "id": "mpf8viawrsib",
+        "title": "04 May 2026 | Botany | Living World | Chapter-1",
+        "subject": "Botany",
+        "ytId": "https://youtu.be/wyjhZ-nq6fA",
+        "batchId": "mpf8m00h475g",
+        "date": "2026-05-21"
     },
     {
-        "id": "moslcql4l2qd",
-        "title": "demo",
-        "subject": "demo",
-        "ytId": "uK7Ovgs44Uk",
-        "batchId": "moslc5mhl5rd",
-        "date": "2026-05-05"
+        "id": "mpf8v1brww2z",
+        "title": "NEET Growth | 04 May 2026 | Chemistry | Some Basic Concepts of Chemistry | Chapter 1",
+        "subject": "Chemistry",
+        "ytId": "https://youtu.be/h6BuHe6Sx-A",
+        "batchId": "mpf8m00h475g",
+        "date": "2026-05-21"
+    },
+    {
+        "id": "mpf8uk6niwuq",
+        "title": "NEET Growth | 04 May 2026 | Zoology | Animal Kingdom | Chapter 1",
+        "subject": "Zoology",
+        "ytId": "https://youtu.be/fCVtCEVdY_E",
+        "batchId": "mpf8m00h475g",
+        "date": "2026-05-21"
+    },
+    {
+        "id": "mpf8u4cc50f7",
+        "title": "NEET Growth | 05 May 2026 | Botany | Living World | Chapter 1",
+        "subject": "Botany",
+        "ytId": "https://youtu.be/jgtOkrRBjIY",
+        "batchId": "mpf8m00h475g",
+        "date": "2026-05-21"
+    },
+    {
+        "id": "mpf8tg1pecha",
+        "title": "NEET Growth | 05 May 2026 | Zoology | Animal Kingdom | Chapter 1",
+        "subject": "Zoology",
+        "ytId": "https://youtu.be/btHCrlOZE80",
+        "batchId": "mpf8m00h475g",
+        "date": "2026-05-21"
     }
 ],
 
