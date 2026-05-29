67b3428f3bf593678b0e41a13539d1dbbf8e0c60
gwalioracademicacademics-prog <gwalioracademicacademics@gmail.com>
2026-05-29 16:32:45 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // ================================================================
 //  data.js — Unacademy Gwalior Database
-//  Generated: 27/05/2026, 22:38:45
+//  Generated: 29/05/2026, 16:32:17
 //  Paste this entire file into data.js on GitHub and commit.
 // ================================================================
 
@@ -42,6 +42,10 @@ const DB = {
     {
         "id": "mpf8mm5qdkpy",
         "name": "Foundation Adapt-1"
+    },
+    {
+        "id": "mpqsbvcv6bz4",
+        "name": "NEET Conquer 2 (Hindi)"
     }
 ],
 
@@ -89,6 +93,302 @@ const DB = {
 ],
 
   lectures: [
+    {
+        "id": "mpqsenl6v36i",
+        "title": "27 May 2026 Dropper NEET 2 Hindi (Physics)",
+        "subject": "Physics",
+        "ytId": "1I9uodnrPqdQ9SsVNyk4tsdNgIkgbO9A2",
+        "batchId": "mpqsbvcv6bz4",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqse5oeyfca",
+        "title": "27 May 2026 Dropper NEET 2 Hindi (Botany)",
+        "subject": "Botany",
+        "ytId": "1oHogmqvxPaxdPXerUOef6XBdIb4eM3DK",
+        "batchId": "mpqsbvcv6bz4",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqscje6rkn7",
+        "title": "25 May 2026 Dropper NEET 2 Hindi (Zoology)",
+        "subject": "Zoology",
+        "ytId": "1yiydS84F9Xq9c0syWSy1yPKcIl6qu2YX",
+        "batchId": "mpqsbvcv6bz4",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqrk4hyi2mh",
+        "title": "27 May 2026 NEET Conquer-1 (Zoology)",
+        "subject": "Zoology",
+        "ytId": "1jCecbvHD7sQBipe0kLeOy4BpXu3T4Qjh",
+        "batchId": "mpf8m5xgxy9t",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqrjqlxg7cx",
+        "title": "27 May 2026 NEET Conquer-1 (Botany)",
+        "subject": "Botany",
+        "ytId": "1VWHs3CV7OA5Lu-RzHcGCHuW1Vhy801bK",
+        "batchId": "mpf8m5xgxy9t",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqrj8qjog57",
+        "title": "26 May 2026 NEET Conquer-1 (Botany)",
+        "subject": "Botany",
+        "ytId": "1thz22Otf-BAUB71xY7yBdHWnnU6ZP_Dq",
+        "batchId": "mpf8m5xgxy9t",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqrit9xecpt",
+        "title": "25 May 2026 NEET Conquer-1 (Zoology)",
+        "subject": "Zoology",
+        "ytId": "1K7dQWciHowVRLR-k6n2ni0dk5pN00mYx",
+        "batchId": "mpf8m5xgxy9t",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqridnfos7f",
+        "title": "25 May 2026 NEET Conquer-1 (Chemistry)",
+        "subject": "Chemistry",
+        "ytId": "14rmm4dVCkcrElciewR4K8euEuRVc2wxA",
+        "batchId": "mpf8m5xgxy9t",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqq93om4gkr",
+        "title": "25 May 2026 (R-3) IIT-JEE Growth-1 (Maths - Fundamental of Mathemataics (L-17))",
+        "subject": "Maths",
+        "ytId": "1sdBHYwUObn65AE8rmEltivanO5lmHAF5",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqq8jmevoxh",
+        "title": "25 May 2026 (R-3) IIT-JEE Growth-1 (Physics - Basic Maths (L-16))",
+        "subject": "Physics",
+        "ytId": "10k-OgrHsJE9shayGCnmzTG_HGQKKZeaA",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqq85rsw2gz",
+        "title": "26 May 2026 IIT-JEE  Growth-1 (Maths - Fundamental of Mathematics L-17)",
+        "subject": "Maths",
+        "ytId": "1M9RvvGOnVhDNwpYQ8GOwszsqEI_vN0gz",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqq7rs3eern",
+        "title": "26 May 2026 IIT-JEE Growth-1 (Chemistry - Atomic Structure L-2).mp4",
+        "subject": "Chemistry",
+        "ytId": "1DRe1p5hZjTt3RWHP4c4xfQkEIGHq1MqT",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqq4d8ftmk4",
+        "title": "26 May 2026 IIT-JEE Growth-1 (Physics - Basic Math L-17)",
+        "subject": "Physics",
+        "ytId": "1sdBHYwUObn65AE8rmEltivanO5lmHAF5",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqpo0t9b78d",
+        "title": "25 May 2026 (R-6) IIT-JEE Excel-2 (Maths - Matrix (L-5))",
+        "subject": "Maths",
+        "ytId": "1x0rxmUNRkoy8twGic6QSDSMqBp20_HME",
+        "batchId": "mpf8lfvkcjdj",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqpnhr103od",
+        "title": "25 May 2026 (R-6) IIT JEE Excel-2 (Physics - Magnetism (L-7))",
+        "subject": "Physics",
+        "ytId": "1HSs38WuvfHmbog8TkQfJ_ptf9edDncgO",
+        "batchId": "mpf8lfvkcjdj",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkzk3t09cd",
+        "title": "14 May 2026 IIT-JEE Growth-1 (Maths - Fundamental of Mathematics)",
+        "subject": "Maths",
+        "ytId": "1HDBX0JL-BwRjld4XrDFEsSIIhcS9Kbfu",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkz77cag5q",
+        "title": "12 May 2026 IIT-JEE Growth-1 (Maths - Fundamental of Mathematics)",
+        "subject": "Maths",
+        "ytId": "18utvKgx3ezWj5ZjEnpJ4m7RKz7RXpJ0S",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkyunx2301",
+        "title": "11 May 2026 IIT-JEE Growth-1 (Maths - Fundamental of Mathematics)",
+        "subject": "Maths",
+        "ytId": "1Gz2parM5Lfsj0ybtviO-uD3hxH6CLOOp",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkyiroaxre",
+        "title": "7 May 2026 IIT-JEE Growth-1 (Physics - Basic Maths)",
+        "subject": "Physics",
+        "ytId": "1zoyFzLmUMtgEml4zjiv8Pll-7SNXLA8m",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqky5uel8e4",
+        "title": "7 May 2026 IIT-JEE Growth-1 (Maths - Fundamental of Mathematics)",
+        "subject": "Maths",
+        "ytId": "1Beu0ES0_Uopf5OPNBBQvWPQrIlGD234M",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkxi64na33",
+        "title": "5 May 2026 IIT-JEE Growth-1 (Physics - Basic Math)",
+        "subject": "Physics",
+        "ytId": "1aQObZ9IPrUZ_-7q5dkwTZ3WeiNLuSyWT",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkw4pxsnlv",
+        "title": "5 May 2026 IIT-JEE Growth-1 (Chemistry - Some Basic Concept of Chemistry)",
+        "subject": "Chemistry",
+        "ytId": "1WxIOLLor_dmaCYI3ZQcF1L-Nw9zpdyOo",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqkvly0frwy",
+        "title": "5 May 2026 IIT JEE Growth-1 (Maths - Fundamental of Mathematics)",
+        "subject": "Maths",
+        "ytId": "1pRNvVLATOhu7r_if3AUoXM249i9fo4rl",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqijggr75ps",
+        "title": "04 May 2026 (Physics - Basic Math-1)-JEE Growth-1",
+        "subject": "Physics",
+        "ytId": "1mUrvBNE58c5CUyH4YHr01JE5kqoCEMoo",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqij3yqyyvq",
+        "title": "04 May 2026 (Maths - Fundamental of Mathematics-1)-JEE Growth-1",
+        "subject": "Maths",
+        "ytId": "1RiRfcOKRzp9VEetdFwTvKwx4q6GmsXV_",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqiiqh2sbr8",
+        "title": "04 May 2026 (Chemistry - Some Basic Concepts of Chemistry-1)-JEE Growth-1",
+        "subject": "Chemistry",
+        "ytId": "1AiVN9yGmrXcK0ZBw1KradsJae5sQig1D",
+        "batchId": "mpf8lr5idez8",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqig69n6kvg",
+        "title": "16 May 2026 Elevate (Physics)",
+        "subject": "Physics",
+        "ytId": "18c3y2jGz9X2vxY1b-uUSYA9Q_ulHK4bb",
+        "batchId": "mpf8mi1kdw37",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqifszf7z5o",
+        "title": "12 May 2026 Elevate (Physics)",
+        "subject": "Physics",
+        "ytId": "1Gd9GAYSTc5HBm1fl7rQZirwP6GXxnyYc",
+        "batchId": "mpf8mi1kdw37",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqifb9kungl",
+        "title": "12 May 2026 Elevate (Maths)",
+        "subject": "Maths",
+        "ytId": "1NtytwCjWEBZAmvGkYH005h8yiY15Wntx",
+        "batchId": "mpf8mi1kdw37",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqieuw50so9",
+        "title": "09 May 2026 Elevate (Physics)",
+        "subject": "Physics",
+        "ytId": "17vPRB7-NMGkkcBrioP3SiaXXEzfY6GSu",
+        "batchId": "mpf8mi1kdw37",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhrygfsebx",
+        "title": "15 May 2026 Adapt (Maths)",
+        "subject": "Maths",
+        "ytId": "1SBJF3eaWA6_XyrMSc16QrEnE9dnMCQ3j",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhqewgcbe0",
+        "title": "15 May 2026 Adapt (Chemistry) Vidushi Mam",
+        "subject": "Chemistry",
+        "ytId": "1FEwMRWGMeBlSOenDEGP3QmuhtI7DxSRv",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhpyupwi5g",
+        "title": "15 May 2026 Adapt (Chemisrty) Foundation Adapt-1",
+        "subject": "Chemistry",
+        "ytId": "1XBuKuM9MXwEbfzxakZiA0zB07kp8oOXM",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhphc8bnh7",
+        "title": "11 May 2026 Adapt (Maths)",
+        "subject": "Maths",
+        "ytId": "1TMQyOfIXB2sIZnIVMcgGtBzQtuhG3-eg",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhp0xysjvk",
+        "title": "11 May 2026 Adapt (Chemistry)",
+        "subject": "Chemistry",
+        "ytId": "1bGcwCKDs9PKjlQk8ltus2z-Y_FWS7Ri8",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhonfsi072",
+        "title": "08 May 2026 Adapt (Maths)",
+        "subject": "Maths",
+        "ytId": "159Pjc6ihAGvEMVG6D4ZQ158lk7U6Tnbh",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
+    {
+        "id": "mpqhnja7je1s",
+        "title": "08 May 2026 Adapt (Chemistry)",
+        "subject": "Chemistry",
+        "ytId": "1zjvN-45nD4mBl83pcqZGjhEyTyZNc3iF",
+        "batchId": "mpf8mm5qdkpy",
+        "date": "2026-05-29"
+    },
     {
         "id": "mpobemrendtg",
         "title": "18 May 2026 (Zoology) Akhand Sir",
